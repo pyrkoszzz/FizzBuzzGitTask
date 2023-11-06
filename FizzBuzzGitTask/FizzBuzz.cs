66e6568ef729988907074b0a0fb06b0aa26560cb
pyrkoszzz <pepexd555@gmail.com>
2023-11-06 15:59:18 +0100

    Create FizzBuzz class

---
+++ FizzBuzzGitTask/FizzBuzz.cs
@@ -0,0 +1,28 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        private enum Constants { Fizz, Buzz };
+        private static bool IsDivisibleByThree(int number) => number % 3 == 0;
+        private static bool IsDivisibleByFive(int number) => number % 5 == 0;
+
+        private static string CheckFizzBuzz(int number)
+        {
+            string result = string.Empty;
+
+            if (IsDivisibleByThree(number))
+                result += Constants.Fizz;
+            if (IsDivisibleByFive(number))
+                result += Constants.Buzz;
+            if (result.Length == 0)
+                result += number.ToString();
+
+            return result;
+        }
+
+        public static void PrintFizzBuzz(int number)
+        {
+            Console.WriteLine(CheckFizzBuzz(number));
+        }
+    }
+}
