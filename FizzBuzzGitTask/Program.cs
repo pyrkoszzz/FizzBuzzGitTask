64936d343ed8f876b567a258bef7712c7fdddc92
pyrkoszzz <pepexd555@gmail.com>
2023-11-08 19:19:19 +0100

    Date test

--- FizzBuzzGitTask/Program.cs
+++ FizzBuzzGitTask/Program.cs
@@ -2,7 +2,7 @@
 {
     static void Main(string[] args)
     {
-        FizzBuzz.FizzBuzzPrinter.PrintFizzBuzz(258);
+        FizzBuzz.FizzBuzzPrinter.PrintFizzBuzz(1337);
         FizzBuzz.FizzBuzzPrinter.PrintFizzBuzzRange(1,100);
     }
 }
